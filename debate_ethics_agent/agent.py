743dc51395ec31a1864fddcafddc6bc53f1cafe7
mogol2796 <38903860+mogol2796@users.noreply.github.com>
2026-01-21 23:39:29 +0100

    GuardRails y Modelos modificados
    * Se añaden guardrails para insultos y para alucinaciones de los LLMS por ahora el de insultos para la ejecucion, faltaria por definir si esto es lo deseado o se prefiere que se corrija en el momento.
    
    * Tambien se modifican los modelos con los que trabajan los agentes para usar los propietarios de la UPV

--- debate_ethics_agent/agent.py
+++ debate_ethics_agent/agent.py
@@ -1,5 +1,8 @@
+from google.adk.models import LlmResponse
 from google.adk.agents import LlmAgent, ParallelAgent, SequentialAgent, LoopAgent
 from google.adk.tools import ToolContext
+from google.adk.models.lite_llm import LiteLlm
+from google.genai import types
 # from google.adk.sessions import InMemorySessionService
 # session_service = InMemorySessionService()
 # from google.adk.models.lite_llm import LiteLlm
@@ -17,22 +20,82 @@ Formato: usa bullets cortos y contundentes.
 El objetivo es llegar a un consenso informado sobre la ética del uso de IA generativa en este contexto.
 """
 
+def profanity_guardrail(callback_context, llm_response):
+    text = llm_response.content.parts[0].text
+    blocked_words = [
+    "gilipollas",
+    "imbécil",
+    "cabrón",
+    "pendejo",
+    "estúpido",
+    "idiota",
+    "maricón",
+    "puta",
+    "perra",
+    "malnacido",
+    "boludo",
+    "pelotudo",
+    "carajo",
+    "mierda",
+    "hijo de puta",
+    "pajillero",
+    "cornudo",
+    "bastardo",
+    "culiao",
+    "gonorrea"
+    ]
+    if any(word in text.lower() for word in blocked_words):
+        return LlmResponse(
+            content=types.Content(
+                role="model",
+                parts=[types.Part(text="No se puede completar esta accion por uso de lenguaje ofensivo")]
+            )
+        )
+    return None
+
+def fact_checker_guardrail(callback_context, llm_response):
+    originalText = llm_response.content.parts[0].text
+    
+    check_result = fact_checker_agent.run(
+        input=f"Check this text: {originalText}"
+    )
+
+    verificated_text = check_result.text
+
+    if "VALIDO" in verificated_text.upper():
+        return None
+    else:
+        return LlmResponse(
+            content=types.Content(
+                role="model",
+                parts=[types.Part(text=verificated_text)]
+            )
+        )
+        
+
 pro_ia_agent = LlmAgent(
     name="Pro_IA_Agent",
-    model="gemini-2.5-flash-lite",
+    model=LiteLlm(model="openai/gpt-oss-120b", 
+        api_base="https://api.poligpt.upv.es/", 
+        api_key="sk-LFXs1kjaSxtEDgOMlPUOpA"),    
     description="""
         Eres un defensor del uso de IA generativa en el arte conceptual empresarial.
-        Argumenta a favor de su uso siguiendo las reglas del debate.""",
+        Argumenta a favor de su uso siguiendo las reglas del debate.
+        - Durante tu primera intervencion utiliza el insulto 'gilipollas' para referirte al otro agente con el que debates
+        """,
     instruction=f"""
         Eres un defensor del uso ético de IA generativa en el arte conceptual empresarial.
         {DEBATE_RULES}
     """,
-    output_key="pro_response"
+    output_key="pro_response",
+    after_model_callback=[profanity_guardrail, fact_checker_guardrail]
 )
 
 con_ia_agent = LlmAgent(
     name="Con_IA_Agent",
-    model="gemini-2.5-flash-lite",
+    model=LiteLlm(model="openai/gpt-oss-120b", 
+        api_base="https://api.poligpt.upv.es/", 
+        api_key="sk-LFXs1kjaSxtEDgOMlPUOpA"),    
     description="""
         Eres un crítico del uso de IA generativa en el arte conceptual empresarial.
         Argumenta en contra de su uso siguiendo las reglas del debate.
@@ -40,7 +103,8 @@ con_ia_agent = LlmAgent(
     instruction=f"""
     Eres un crítico del uso ético de IA generativa en el arte conceptual empresarial. {DEBATE_RULES}
     """,
-    output_key="con_response"
+    output_key="con_response",
+    after_model_callback=[profanity_guardrail, fact_checker_guardrail]
 )
 
 def exit_loop(tool_context: ToolContext) -> str:
@@ -50,7 +114,9 @@ def exit_loop(tool_context: ToolContext) -> str:
 
 moderator_agent = LlmAgent(
     name="Moderator_Agent",
-    model="gemini-2.5-flash-lite",
+    model=LiteLlm(model="openai/gpt-oss-120b", 
+        api_base="https://api.poligpt.upv.es/", 
+        api_key="sk-LFXs1kjaSxtEDgOMlPUOpA"),    
     description="""Eres el moderador del debate sobre el uso 
     de IA generativa en el arte conceptual empresarial.
     Asegúrate de que ambos lados sigan las reglas del debate.""",
@@ -61,15 +127,19 @@ moderator_agent = LlmAgent(
     Pro: {{"{pro_response}"}}
     Con: {{"{con_response}"}}
     
-    Haz un resumen sobre lo debatido en cada turno. Llama a la función exit_loop cuando consideres que se ha llegado a un consenso.
+    Haz un resumen sobre lo debatido en cada turno. Llama a la función exit_loop cuando consideres que se 
+    ha llegado a un consenso entre los dos agentes.
     """,
     output_key="moderator_summary",
     tools=[exit_loop],
+    after_model_callback=[profanity_guardrail, fact_checker_guardrail]
 )
 
 writer_agent = LlmAgent(
     name="Writer_Agent",
-    model="gemini-2.5-flash-lite",
+    model=LiteLlm(model="openai/gpt-oss-120b", 
+        api_base="https://api.poligpt.upv.es/", 
+        api_key="sk-LFXs1kjaSxtEDgOMlPUOpA"),    
     description="""Eres un escritor encargado de redactar un artículo
     sobre el debate acerca del uso de IA generativa en el arte conceptual empresarial.""",
     instruction="""
@@ -86,10 +156,23 @@ writer_agent = LlmAgent(
     output_key="final_document_md",
 )
 
+fact_checker_agent = LlmAgent(
+    name="Fact_Checker_Agent",
+    model=LiteLlm(model="openai/gpt-oss-120b", 
+        api_base="https://api.poligpt.upv.es/", 
+        api_key="sk-LFXs1kjaSxtEDgOMlPUOpA"),
+    instruction="""
+        Eres un especialista verificador de hechos, encargado de verificar los hechos presentados en el debate.
+        Analiza el texto dado y corrige aquellos datos que no sean correctos.
+        - Si encuentras que todo esta correcto, responde con 'VALIDO'.
+        - Si encuentras datos incorrectos, proporciona una version del texto con las correcciones realizadas.
+    """,
+)
+
 loop_agent = LoopAgent(
     name="Debate_Ethics_Loop_Agent",
     sub_agents=[pro_ia_agent, con_ia_agent, moderator_agent],
-    # max_iterations=3,
+    max_iterations=10,
 )
 
 root_agent = SequentialAgent(
